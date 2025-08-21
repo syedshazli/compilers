e80756f140e54dd36555069cf34c415d98908080
syedshazli <syed.abdu1818@gmail.com>
2025-08-21 18:11:19 -0400

    consider using sets instead of enums

--- Scanners/Recognizer/RecognizerExample.cpp
+++ Scanners/Recognizer/RecognizerExample.cpp
@@ -29,20 +29,23 @@ int main(int argc, char **argv){
      */
 
 
-    char firstChar = argv[1][0]; // FIXME: 1 indexed because first is the executable (I think, need to check)
+    char currentChar = argv[1][0]; // FIXME: 1 indexed because first is the executable (I think, need to check)
     state myState;
     myState = s0;
     if(sizeof(argv[1]) > 6 || sizeof(argv[1]) == 0)
     {
         myState = serror;
     }
-
+    
+    int i = 0;
     while(myState != serror && character isnt the last character )
     {
-      
+      state = getCurrentState() //FIXME: SHOULD BE IMPLEMENTED
+      i +=1;
+      currentChar = argv[1][i];
     }
 
-    if(myState is in acceptedStates )
+    if(myState is in acceptedStates ) // FIXME: Figure out how to check if two enum vals are same.. or replace the enums with sets?
     {
         return true;
     }
