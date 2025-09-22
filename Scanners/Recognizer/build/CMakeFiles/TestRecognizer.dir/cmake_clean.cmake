ba5c6e4566a495e5e8845f5b98eb5a63b6a3dc2b
syedshazli <syed.abdu1818@gmail.com>
2025-09-22 11:51:07 -0400

    CMAKE

---
+++ Scanners/Recognizer/build/CMakeFiles/TestRecognizer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TestRecognizer.dir/Tests/testRecognizer.cpp.o"
+  "CMakeFiles/TestRecognizer.dir/Tests/testRecognizer.cpp.o.d"
+  "TestRecognizer"
+  "TestRecognizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TestRecognizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
