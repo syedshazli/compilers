33a432b57918f6c467eb4e380088651c282970a8
syedshazli <syed.abdu1818@gmail.com>
2025-10-06 18:41:39 -0400

    more skeletons

--- CFG/MyCFG/CFG.cpp
+++ CFG/MyCFG/CFG.cpp
@@ -1,35 +1,59 @@
 // input, a CFG provided by the user (make it arrays of states or something)
-// output: A Tree of the data
+// output: A Tree of the program
 #include <iostream>
 #include <set>
+#include <string>
 
 struct CFG{
 
-    std::set<string> states; 
+    std::set<std::string> states; 
 
 };
 
-CFG getUserInput(char *input)
+class Tree{
+
+};
+
+CFG convertInputToCFG(char **input)
 {
 
 }
 
 // FIXME: Type might not need to be CFG's
-bool convertCFGToRules(CFG userCFG, std::set <CFG> outputRules)
+bool convertCFGToRules(CFG userCFG, std::set <CFG> &outputRules)
 {
 
 }
-int main(int argc, char* argv)
+bool convertCFGToTree(std::set <CFG> outputRules, Tree &userTree)
 {
-    CFG userCFG = getUserInput(char *argv);
+
+}
+void printTree(Tree CFGTree)
+{
+
+}
+void printErrorMessage()
+{
+    std::cout<<"Sorry, we couldn't generate a CFG for you. Please fix and try again.";
+}
+int main(int argc, char** argv)
+{
+    CFG userCFG = convertInputToCFG(argv);
     std::set <CFG> outputRules;
+
     if (!convertCFGToRules(userCFG, outputRules))
     {
-        return;
+        printErrorMessage();
     }
 
-    convertCFGToTree();
 
-    printTree();
+    // FIXME: Add inputs to function
+    Tree myTree;
+    if (!convertCFGToTree(outputRules, myTree))
+    {
+        printErrorMessage();
+    }
+
+    printTree(myTree);
 
 }
